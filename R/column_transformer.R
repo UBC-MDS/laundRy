2bb6be875eeac78015e1d3da90a0dfa5e3268510
Arun Marria <arun.marria@gmail.com>
2020-02-26 21:30:37 -0800

    column transformer function's structure added

--- R/column_transformer.R
+++ R/column_transformer.R
@@ -1,1 +1,17 @@
-#test
+#' Transforms categorical and numerical features
+#'
+#' @param df dataframe to be transformed
+#' @param num_list list of columns to have numerical transformation   to be applied
+#' @param cat_list list of columns to have categorical transformations to be applied
+#' @param num_trans method for numerical transformation (default = "standard scaling")
+#' @param cat_trans method for categorical transformation (default = "ohe")
+#'
+#' @return df, with tranformed column values
+#'
+#' @examples
+#' column_transformer(df, list('weight'), list('education'))
+#'
+#' @export
+column_transformer <-
+  function(df, num_list, cat_list, num_trans, cat_trans)
+  { }
