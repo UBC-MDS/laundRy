092999fbc41e5e0e9daa430f0a509bfd47cc86ed
Alexander Hinton <ahinton1991@gmail.com>
2020-02-28 11:54:49 -0800

    edit fill missing func and man page

--- R/fill_missing.R
+++ R/fill_missing.R
@@ -1,15 +1,19 @@
 #' Handles missing values in a dataframe
 #'
+#' Replace missing values in dataframe columns by the specified methods. 
+#' Separate methods can be applied for categorical column imputation and 
+#' numerical column imputation. 
+#' 
 #' @param df dataframe to be transformed
 #' @param num_list list of columns to have numerical imputation applied
 #' @param cat_list list of columns to have categorical imputation applied
-#' @param num_imp method for numerical imputation (default = "mean")
-#' @param cat_imp method for categorical imputation (default = "mode")
+#' @param num_imp method for numerical imputation
+#' @param cat_imp method for categorical imputation
 #'
 #' @return df, with missing values replaced by the specified method
 #'
 #' @examples
-#' fill_missing(df, list('weight'), list('education'))
+#' fill_missing(df, list('weight'), list('education'), 'mean', 'mode')
 #'
 #' @export
 fill_missing <-
