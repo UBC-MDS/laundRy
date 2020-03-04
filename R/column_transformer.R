c4f454916dd9a915442165a787aadfe0758256c2
Arun Marria <arun.marria@gmail.com>
2020-03-03 20:31:25 -0800

    structure of function added

--- R/column_transformer.R
+++ R/column_transformer.R
@@ -1,6 +1,28 @@
 # importing required libraries
 library(caret)
-library(tidyverse)
+library(tidyr)
+library(dplyr)
+library(CatEncoders)
+
+# dataframes for testing
+employee_name <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")
+manager <- c("M1", "M2", "M3", "M1", "M2", "M3", "M1", "M2", "M3", "M1")
+age <- c(23, 56,34,40, 34,56, 45, 65, 54,43)
+sex <- c('M', 'F','M', 'F','M', 'F','M', 'F', 'M', 'F' )
+daily_wage<- c(100,200, 100, 60, 80, 140, 320,60, 90, 90)
+x_train <- data.frame(employee_name, manager, age, sex, daily_wage)
+
+
+employee_name <- c("K", "L", "M", "N", "O", "P")
+manager <- c("M1", "M2", "M3", "M1", "M2", "M3")
+age <- c(23, 56,34,40, 34,56)
+sex <- c('M', 'F','M', 'F','M', 'F')
+daily_wage<- c( 80, 140, 320,60, 90, 90)
+x_test <- data.frame(employee_name, manager, age, sex, daily_wage)
+
+col_names <- list(cat_cols= c("manager", "sex"), num_cols = c("age", "daily_wage" ))
+
+
 
 #' Transforms columns of a dataframe
 #'
@@ -9,44 +31,86 @@ library(tidyverse)
 #' numerical column transformation.
 #'
 #'
-#' @param df dataframe to be transformed
-#' @param num_vec list of columns to have numerical transformation   to be applied
-#' @param cat_vec list of columns to have categorical transformations to be applied
-#' @param num_trans method for numerical transformation (default = "standard_scaling")
-#' @param cat_trans method for categorical transformation (default = "ohe")
+#' @param x_train trainingset dataframe/tibble
+#' @param x_test test set dataframe/tibble
+#' @param col_names list of names of categorical columns
+#' @param num_trans method(character) for numerical transformation (default = "standard_scaling")
+#' @param cat_trans method(character) for categorical transformation (default = "ohe")
 #'
-#' @return df, with tranformed column values
+#' @return x_train,x_test transformed
 #'
 #' @examples
-#' column_transformer(df, list('weight'), list('education'))
+#' column_transformer(x_train, list('weight'), list('education'))
 #'
 #' @export
-#'
+
+
+
 column_transformer <-
-  function(df, num_vec, cat_vec, num_trans, cat_trans)
+  function(x_train,x_test, col_names, num_trans="standard_scaling", cat_trans="onehot_encoding")
   {
+
+    # block to ensure consistency in naming convention
+    x_train = x_train
+    x_test = x_test
+    num_cols = col_names$num_cols
+    cat_cols = col_names$cat_cols
+
+
+
 
     # numeric column transformation
 
     if(num_trans == "standard_scaling"){
-      preProcValues = preProcess(df[,num_vec], method = c("center", "scale"))
+      preProcValues <- preProcess(x_train[,num_cols], method = c("center", "scale"))
+      x_train[,num_cols] = predict(preProcValues, x_train[,num_cols])
+      x_test[,num_cols] = predict(preProcValues, x_test[,num_cols])
 
-    }
-    if(num_trans == "minmax_Scaling"){
-      preProcValues = preProcess(df[,num_vec], method = "range")
 
-    }
-    else{
+    }else if(num_trans == "minmax_scaling"){
+      preProcValues = preProcess(x_train[,num_cols], method = "range")
+      x_train[,num_cols] = predict(preProcValues, x_train[,num_cols])
+      x_test[,num_cols] = predict(preProcValues, x_test[,num_cols])
+
+    }else{
       print("Invalid transformation type for numerical columns")
     }
 
-    df[,num_vec] = predict(preProcValues, df[,num_vec])
 
 
     # transformation for categorical columns
-    if(cat_trans == 'onehot'){
-      dmy <- dummyVars(" ~ .", data = df[,cat_vec], fullRank  = TRUE)
+    if(cat_trans == 'onehot_encoding'){
+      x_train_cat <- x_train[, cat_cols]
+      x_test_cat <- x_test[, cat_cols]
+      x_train <- x_train[ , !(names(x_train) %in% cat_cols)]
+      x_test <- x_test[ , !(names(x_test) %in% cat_cols)]
+
+      dmy <- dummyVars(" ~ .", data = x_train_cat, fullRank  = TRUE)
+      x_train_cat <- data.frame(predict(dmy, newdata = x_train_cat))
+
+      x_test_cat <- data.frame(predict(dmy, newdata = x_test_cat))
+
+      x_train <- cbind(x_train, x_train_cat )
+      x_test <- cbind(x_test, x_test_cat)
+
+    }
+    else if(cat_trans=='label_encoding'){
+      for (col in cat_cols) {
+
+          x_train[[col]] = as.character(x_train[[col]])
+          x_test[[col]] = as.character(x_test[[col]])
+          levels <- sort(unique(c(x_train[[col]])))
+          x_train[[col]]  <- as.integer(factor( x_train[[col]] , levels = levels))
+          x_test[[col]]<- as.integer(factor(x_test[[col]], levels = levels))
+        }
+    }
+    else{
+      print("Invalid input for categorical transformation")
     }
 
+    return(list (x_train = x_train,x_test = x_test))
 
-    }
+
+      }
+
+
