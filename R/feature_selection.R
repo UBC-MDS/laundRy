bcc76eb8d3c73f7b0ff50da805f9593d6eb168e9
garg1234 <gargkaman7@gmail.com>
2020-03-04 21:51:32 -0800

    Add select features function and tests

--- R/feature_selection.R
+++ R/feature_selection.R
@@ -14,6 +14,44 @@
 #' @export
 #' @examples
 #' feature_selection(data.frame(X1 = c(2,4,3), X2 = c('c','d','c')),c(4,3,5),"regression",2)
+
+library(caret)
 feature_selection <- function(X,y,mode,n_features) {
-  pass
+
+
+  if (mode == "regression") {
+
+    control <- rfeControl(functions = lmFuncs,
+                          method = "cv", #cross validation,
+                          returnResamp = "all",
+                          verbose = FALSE, #prevents copious amounts of output from being produced.
+    )
+
+    lm_m <- rfe(X,
+                y,
+                sizes = c(1:ncol(X)),
+                rfeControl = control)
+
+    tf <- lm_m$variables$var[1:n_features]
+
+
+  } else {
+
+    control <- rfeControl(functions = lrFuncs,
+                          method = "cv", #cross validation,
+                          returnResamp = "all",
+                          verbose = FALSE, #prevents copious amounts of output from being produced.
+    )
+
+    lr_m <- rfe(X,
+                y,
+                sizes = c(1:ncol(X)),
+                rfeControl = control)
+
+
+    tf <- lr_m$variables$var[1:n_features]
+  }
+
+  return (tf)
+
 }
--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,5 @@ library(testthat)
 library(laundRy)
 
 test_check("laundRy")
+
+
