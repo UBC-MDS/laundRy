9d2bcdcc68b16ebf0d007de272429392bac01e49
Cari <cari.gostic@gmail.com>
2020-03-20 11:32:48 -0700

    Address categorize feedback from Brayden

--- R/categorize.R
+++ R/categorize.R
@@ -9,17 +9,20 @@
 #' @param max_cat int, the maximum number of unique values that
 #' define a categorical column
 #'
-#' @return list with sub vectors named 'numeric' and 'categorical',
+#' @return list with char vectors named 'numeric' and 'categorical',
 #' containing column names of each type
 #' @export
 #' @examples
-#' categorize(data.frame(a = c(1.2,2.3,3.4), b = c('a','b','c')))
+#' categorize(data.frame(a = c(1.2, 2.3, 3.4), b = c('a', 'b', 'c')))
 
 categorize <- function(df, max_cat = 10) {
   # Check that inputs are valid
-  if(class(max_cat) != 'numeric') stop("Error: max_cat must be a positive integer")
-  if(max_cat%%1 != 0 | max_cat < 1) stop("Error: max_cat must be a positive integer")
-  if(class(df) != 'data.frame') stop("Error: Input for df must be of class data.frame")
+  (if(class(max_cat) != 'numeric')
+    stop("Error: max_cat must be a positive integer"))
+  (if(max_cat%%1 != 0 | max_cat < 1)
+    stop("Error: max_cat must be a positive integer"))
+  (if(class(df) != 'data.frame')
+    stop("Error: Input for df must be of class data.frame"))
 
 
   # Track all columns and their types
@@ -30,11 +33,13 @@ categorize <- function(df, max_cat = 10) {
   categorical <- c(names(col_types[col_types == 'factor']))
   # Add columns to list with fewer than or equal to max_cat unique values
   unique_vals <- sapply(df, function(x) length(unique(x)))
-  categorical <- append(categorical, names(unique_vals[unique_vals <= max_cat]))
+  categorical <- (append(categorical,
+                         names(unique_vals[unique_vals <= max_cat])))
 
   # Mark numeric columns
   # All columns of type numeric that aren't already marked categorical
-  numeric <- names(col_types[col_types == 'numeric' & !(names(col_types) %in% categorical)])
+  numeric <- (names(col_types[col_types == 'numeric' &
+                                !(names(col_types) %in% categorical)]))
 
   # Return list of vectors with duplicates removed
   return(list(numeric=unique(numeric), categorical=unique(categorical)))
--- tests/testthat/test_feature_selection.R
+++ tests/testthat/test_feature_selection.R
@@ -20,8 +20,8 @@ test_1()
 
 test_2 <- function(){
   test_that("Test result for regression", {
-    X = generate_data_regression()[[1]]
-    y = generate_data_regression()[[2]]
+    X <- generate_data_regression()[[1]]
+    y <- generate_data_regression()[[2]]
 
     expect_true(feature_selection(X,y,"regression",1) == "x1")
 
@@ -34,8 +34,8 @@ test_2()
 
 test_3 <- function(){
   test_that("Test result for classification", {
-    X = generate_data_classification()[[1]]
-    y = generate_data_classification()[[2]]
+    X <- generate_data_classification()[[1]]
+    y <- generate_data_classification()[[2]]
     expect_true(feature_selection(X,y,"classification",1) == "x2")
 
   })
@@ -46,8 +46,8 @@ test_3()
 
 test_4 <- function(){
   test_that("Test result for Data Frame check", {
-    X = generate_data_wrong()[[1]]
-    y = generate_data_wrong()[[2]]
+    X <- generate_data_wrong()[[1]]
+    y <- generate_data_wrong()[[2]]
     expect_error(feature_selection(X,y,"classification",1) == "Input Data is not Data Frame")
 
   })
@@ -58,8 +58,8 @@ test_4()
 
 test_5 <- function(){
   test_that("Test result for Data Frame check", {
-    X = generate_data_wrong_one()[[1]]
-    y = generate_data_wrong_one()[[2]]
+    X <- generate_data_wrong_one()[[1]]
+    y <- generate_data_wrong_one()[[2]]
     expect_error(feature_selection(X,y,"classification",1) == "Input Data is not Data Frame")
 
   })
