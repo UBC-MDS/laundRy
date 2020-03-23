9dd2af9ff7647bcdcdbe297b6f68c77434335302
Alexander Hinton <ahinton1991@gmail.com>
2020-03-22 20:35:03 -0700

    update function per Brayden suggestion ref #65

--- R/fill_missing.R
+++ R/fill_missing.R
@@ -14,18 +14,20 @@ getmode <- function(v) {
 #'
 #' @param x_train training set dataframe to be transformed
 #' @param x_test test set dataframe to be transformed
-#' @param column_list named list of columns with two sub vectors, must be named
+#' @param column_list named list of columns with two character vectors, must be named
 #' 'numeric' and 'categorical'
-#' @param num_imp method for numerical imputation (default = "mean")
-#' @param cat_imp method for categorical imputation
+#' @param num_imp method for numerical imputation, options are "mean and" median
+#' @param cat_imp method for categorical imputation, only option is "mode"
 #'
-#' @return list, with missing values replaced by the specified method
+#' @return named list, with two vectors: "x_train", the training set with
+#' missing values filled, and "x_test", the test set with missing values filled
 #' @importFrom magrittr %>%
 #' @importFrom rlang :=
 #' @examples
 #' x_tr <- data.frame('x' = c(2.5, 3.3, NA), 'y' = c(1, NA, 1))
 #' x_test <- data.frame('x' = c(NA), 'y' = c(NA))
-#' fill_missing(x_tr, x_test, list("numeric" = c('x'), "categorical" = c('y')), 'mean', 'mode')
+#' fill_missing(x_tr, x_test, list("numeric" = c('x'),
+#'  "categorical" = c('y')), 'mean', 'mode')
 #'
 #' @export
 fill_missing <- function(x_train, x_test, column_list, num_imp, cat_imp)
@@ -71,7 +73,8 @@ fill_missing <- function(x_train, x_test, column_list, num_imp, cat_imp)
   # Imputation methods for numerical columns
   for (column in column_list$"numeric"){
     if (num_imp == "mean"){
-      train_col_mean <- x_train %>% dplyr::select(column) %>% dplyr::pull() %>% mean(na.rm = TRUE)
+      train_col_mean <- x_train %>% dplyr::select(column) %>%
+        dplyr::pull() %>% mean(na.rm = TRUE)
       # impute training mean to train column
       x_train <- x_train %>%
         dplyr::mutate(!!column := ifelse(is.na(!!rlang::sym(column)),
@@ -85,7 +88,8 @@ fill_missing <- function(x_train, x_test, column_list, num_imp, cat_imp)
     }
 
     if (num_imp == "median"){
-      train_col_med <- x_train %>% dplyr::select(column) %>% dplyr::pull() %>% stats::median(na.rm = TRUE)
+      train_col_med <- x_train %>% dplyr::select(column) %>%
+        dplyr::pull() %>%  stats::median(na.rm = TRUE)
       # impute training median to train column
       x_train <- x_train %>%
         dplyr::mutate(!!column := ifelse(is.na(!!rlang::sym(column)),
@@ -101,7 +105,8 @@ fill_missing <- function(x_train, x_test, column_list, num_imp, cat_imp)
 
   # Imputation methods for categorical columns
   for (column in column_list$"categorical"){
-    train_col_mode <- x_train %>% dplyr::select(column) %>% dplyr::pull() %>% getmode()
+    train_col_mode <- x_train %>% dplyr::select(column) %>%
+                      dplyr::pull() %>% getmode()
     # impute training mode to train column
     x_train <- x_train %>%
       dplyr::mutate(!!column := ifelse(is.na(!!rlang::sym(column)),
